6c85a8e759833c65dbec362a2c9cadfaece48796
administrador <administrador@m72448.local>
2018-07-18 09:56:58 -0300

    Criação da tela do login

--- AgendaTelefonica/Application/Login/Controllers/ViewController.swift
+++ AgendaTelefonica/Application/Login/Controllers/ViewController.swift
@@ -21,11 +21,14 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        self.btnEntrar.setTitle("Entrar", for: .normal)
+        self.txtEmail.placeholder = L10n.Login.email
+        self.txtSenha.placeholder = L10n.Login.senha
+        
+        self.btnEntrar.setTitle(L10n.Login.entrar, for: .normal)
         self.btnEntrar.layer.cornerRadius = self.btnEntrar.bounds.height / 2
         self.btnEntrar.backgroundColor = UIColor(red: 173/255, green: 216/255, blue: 230/255, alpha: 1)
         
-        self.btnCadastrar.setTitle("Cadastrar", for: .normal)
+        self.btnCadastrar.setTitle(L10n.Login.cadastrar, for: .normal)
         self.btnCadastrar.layer.cornerRadius = self.btnCadastrar.bounds.height / 2
         self.btnCadastrar.backgroundColor = UIColor(red: 173/255, green: 216/255, blue: 230/255, alpha: 1)
         
@@ -36,6 +39,7 @@ class ViewController: UIViewController {
     }
     
     @IBAction func cadastrar(_ sender: Any) {
+    
     }
     
 }
